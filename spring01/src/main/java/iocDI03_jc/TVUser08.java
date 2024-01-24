86760bec54ae5a620f74dad074dd9bcd98519147
User -2023FIULM\Administrator <img4029@naver.com>
2024-01-24 13:59:49 +0900

    240124 점심 정기 패치

--- spring01/src/main/java/iocDI03_jc/TVUser08.java
+++ spring01/src/main/java/iocDI03_jc/TVUser08.java
@@ -1,34 +1,12 @@
-package iocDI02_anno;
+package iocDI03_jc;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
-import org.springframework.context.support.GenericXmlApplicationContext;
-import org.springframework.stereotype.Component;
 
-//** Annotation, @, 애너테이션
-//=> 컴파일러에게 알려줌 (지시어)
-
-//** @ Annotation 애너테이션 활용방법 *********************
-
-//1. xml 에 context 네임스페이스 추가 [NameSpaces] 이용
-//=> Component-scan 설정  : 
-//<context:component-scan base-package="d0714.iocEx05" />
-//  
-//2. 소스코드 
-//=> import 확인  : org.springframework.context.support.*;
-//=> 생성을 원하는 TV 클래스 위에 @Component("tv") 설정
-
-//=> @Component("tv") 
-//<bean ....> </bean> -> xml
-//new 생성자() -> Java Code
-
-//3.Test : 오류 메시지 확인 하기
-//=> @ Test1 생성 -> <bean ... />
-//=> @ Test2 id명 미 지정(둘다 beanName 없이) 
-//=> ...NoSuchBeanDefinitionException: 
-//           No bean named 'tv' is defined ....
-//=> @ Test3 id명 중복되는 경우 (둘다 beanname  ("tv") 지정 )  
-//=> ...annotation.ConflictingBeanDefinitionException: ....
-//  ...non-compatible bean definition of same name and class ...
+// ** Java Bean Configuration class를 이용한 DI
+// => Bean 컨테이너 : AnnotationConfigApplicationContext 사용
+//                 매개변수로 Java_config_class 를 사용 (JavaConfig01.class)           
+// => Test01 : 스피커 없는 TV
 
 interface TV {
 	void powerOn();
@@ -37,7 +15,6 @@ interface TV {
 	void volumeDown();
 }
 
-//@Component("tv")
 class SsTVi implements TV {
 	SsTVi(){ System.out.println("~~ SsTVi 기본생성자 ~~"); }
 	@Override
@@ -50,7 +27,6 @@ class SsTVi implements TV {
 	public void volumeDown() { System.out.println("~~ SsTVi volumeDown"); }
 }
 
-//@Component("ltv")
 class LgTVi implements TV {
 	LgTVi(){ System.out.println("~~ LgTVi 기본생성자 ~~"); }
 	@Override
@@ -63,13 +39,16 @@ class LgTVi implements TV {
 	public void volumeDown() { System.out.println("~~ LgTVi volumeDown"); }
 }
 
-
-
-public class TVUser05 {
+public class TVUser08 {
 
 	public static void main(String[] args) {
 		// 1. BeanFactory(스프링 컨테이너) 생성
-		AbstractApplicationContext sc = new GenericXmlApplicationContext("iocDI02_anno/app05.xml");
+	    // => AnnotationConfigApplicationContext 사용
+	    //    매개변수로 Java_Config_class 를 사용 (JavaConfig01.class) 
+	    //    -> 주의사항 : "" 없이 화일명 적용, 같은 package에 있으므로  package명 생략
+	    //    -> 참고 : 스프링 프로젝트의 .class 화일 위치 C:\MTest\myWork\Spring01\target\classes\iocDI03_jc
+		
+		AbstractApplicationContext sc = new AnnotationConfigApplicationContext(JavaConfig01.class);
 
 		// 2. 필요한 객체를 전달받고 서비스 실행
 		TV tv = (TV) sc.getBean("tv");
